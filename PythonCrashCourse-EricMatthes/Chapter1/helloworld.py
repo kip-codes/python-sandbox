069620af0352049c7ea2176c526e8b55ebe90041
Kevin Ip <kevin.ip.j@gmail.com>
2017-09-19 15:42:45 -0700

    Messing around with variables

--- PythonCrashCourse-EricMatthes/Chapter1/helloworld.py
+++ PythonCrashCourse-EricMatthes/Chapter1/helloworld.py
@@ -1,1 +1,11 @@
 print("Hello Python interpreter!")
+print("Hello world.")
+
+message = "Hello Python world."
+print(message)
+
+# Using the title() function, we can combine variables and uppercase the first letter of each word.
+first_name = "ada"
+last_name = "lovelace"
+full_name = (first_name + " " + last_name).title()
+print(full_name)
