c77ec635d3c8cf60a069f48fe433b4671619714f
Kevin Ip <kevin.ip.j@gmail.com>
2017-09-23 17:32:46 -0700

    Checking new branch commits

---
+++ PythonCrashCourse-EricMatthes/Chapter1/simple_message.py
@@ -0,0 +1,17 @@
+# File: simple_message
+# Project: PythonCrashCourse
+# Local author: kevinip
+# Created on 09/23/2017 at 5:17 PM
+#
+# For inquiries about the file please contact the author.
+
+# Assignment prompt
+print("2-1. Simple Message:")
+print("Store a message in a variable, and then print that message. (p. 23)\n")
+
+# Begin solution
+message = "Are we clear?"
+print(message)
+
+
+# End solution
---
+++ PythonCrashCourse-EricMatthes/Chapter1/simple_messages.py
@@ -0,0 +1,19 @@
+# File: simple_messages
+# Project: PythonCrashCourse
+# Local author: kevinip
+# Created on 09/23/2017 at 5:28 PM
+#
+# For inquiries about the file please contact the author.
+
+# Assignment prompt
+print("2-2. Store a message in a variable, and print that message.\n"
+      "Then change the value of your variable to a new message, and print the new message. (p. 23)\n")
+
+# Begin solution
+message = "The secret is hidden."
+print(message)
+message = "My secret has been revealed."
+print(message)
+
+
+# End solution
