4d0c0e4fdeda7adcf1bccc78ccad9c18791cf2cd
Kevin Ip <kevin.ip.j@gmail.com>
2017-09-24 01:45:04 -0700

    tiy 4-10 done

---
+++ PythonCrashCourse-EricMatthes/Chapter4/slicing_lists.py
@@ -0,0 +1,27 @@
+# File: slicing_lists
+# Project: PythonCrashCourse
+# Local author: kevinip
+# Created on 09/24/2017 at 1:34 AM
+#
+# For inquiries about the file please contact the author.
+
+# Assignment prompt
+print('* ' *20)
+print("TiY 4-10. Slices:\n")
+
+toppings = ["mushrooms","sausage","cheese","garlic"]
+
+print("The first three items in the list are:")
+print(toppings[:3])
+
+print("The items from the middle of the list are:")
+print(toppings[1:-1])
+
+print("The last three items in the list are:")
+print(toppings[-3:])
+
+
+# Assignment prompt
+print('\n')
+print('* ' *20)
+print("TiY 4-11. My Pizzas, Your Pizzas:\n")
