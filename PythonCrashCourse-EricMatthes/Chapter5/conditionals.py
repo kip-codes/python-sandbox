41a3be5cfeaa5faceb6ba9dcd7f3cc9bbf1a08c9
Kevin Ip <kevin.ip.j@gmail.com>
2017-09-28 16:42:51 -0700

    Messing around with variables

---
+++ PythonCrashCourse-EricMatthes/Chapter5/conditionals.py
@@ -0,0 +1,17 @@
+# File: conditionals
+# Project: PythonCrashCourse
+# Local author: kevin
+# Created on 09/28/2017 at 1:29 PM
+#
+# For inquiries about the file please contact the author.
+import random
+
+
+# Assignment prompt
+print('* ' * 20)
+tiy5_1 = "5-1. Conditional Tests:\nWrite a series of conditional tests. Prince a statement describing each test and your prediction for " \
+         "the result of each test.\nCreate at least 10 tests, with at least 5 evaluating to True and rest to false.\n"
+print(tiy5_1)
+
+rnums = [ for x in range(10)]
+print("Is ")
