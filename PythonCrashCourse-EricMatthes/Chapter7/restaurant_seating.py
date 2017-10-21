7c80727a0eeeb8ad2c6b94efdc3868c050557cfb
Kevin Ip <kevin.ip.j@gmail.com>
2017-10-20 23:27:30 -0700

    revised conditionals for party size

--- PythonCrashCourse-EricMatthes/Chapter7/restaurant_seating.py
+++ PythonCrashCourse-EricMatthes/Chapter7/restaurant_seating.py
@@ -20,7 +20,7 @@ print("In this scenario, we assume that the restaurant is fully automated and en
       "appropriate table.")
 
 # BEGIN SOLUTION
-import time, threading, random
+import time, threading, random, logging, sys
 
 # ERROR CODES
 # 1: success
@@ -77,23 +77,30 @@ def printLargerRooms(partySize):
 def printQueuedMsgs():
     global alerts
 
-    if len(alerts) > 1:
-        for alert in alerts:
-            print(alert)
+    for alert in alerts:
+        print(alert)
+    del alerts[:]
 
 
 # takePartySize(): Take input for party size
 def takePartySize():
+    global areas
+
     while True:
         try:
             ps = int(input("\nAnd how big is your party: "))
-            if not (ps > 0):
+            if ps < 0:
                 raise ValueError()
         except ValueError:
             print("Unacceptable party size. Please try again.")
-        else:  # success
-            print("Ok, party of " + str(ps) + "...")
-            break
+        else:  # valid integer
+            if ps > max(areas.values()):
+                print("I'm sorry, your party is too large to be put under one name."
+                      " Please try again with a smaller party.")
+            else:
+                print("Ok, party of " + str(ps) + "...")
+                printQueuedMsgs()
+                break
     return ps
 
 
@@ -103,7 +110,7 @@ def checkSeats(partySize, name, locPref):
 
     # If the customer's preference will not allow for their party size
     if locPref != 'any' and getAreaCap(locPref) < partySize:
-        print("I'm sorry, our tables in the {} cannot seat {} people. Might I recommend you to some areas that can "
+        print("I'm sorry, our tables in the {0} cannot seat {1} people. Might I recommend you to some areas that can "
               "seat your party?".format(locPref, partySize))
         printLargerRooms(partySize)
         return 2
@@ -118,6 +125,7 @@ def checkSeats(partySize, name, locPref):
         for t in tables:
             if tables[t]['seated'] == 0 and tables[t]['ready'] >= partySize:  # a table is cleaned and prepared for a customer
                 print("Alright " + name.title() + ", our table in the " + tables[t]['location'] + " is ready for you.")
+                printQueuedMsgs()
                 return t
 
     # If nothing is available for them
@@ -125,7 +133,8 @@ def checkSeats(partySize, name, locPref):
         print("I'm sorry " + name.title() + ", but our tables in the " + locPref + " are currently occupied.")
         return 2
     else:  # locPref is any
-        print("Please try again, none of our tables are ready!")
+        print("There's currently an estimated waiting period of {} seconds for a table of your size, so please put "
+              "your name down and we'll seat you as soon as possible.".format(random.randint(5, 15)))
         return 2
 
 
@@ -134,12 +143,13 @@ def checkout(table):
     global tables, alerts
 
     # print(tables[table])
-    alerts.append("\n>>> {0} has paid their bill of {1:.2f}, and table {2} in the {3} has been cleared."
-                  .format(tables[table]["customer"].title(), random.uniform(50, 150), table
+    alerts.append(">>> {0} has paid their bill of ${1:.2f}, and table {2} in the {3} has been cleared."
+                  .format(tables[table]["customer"].title(), random.uniform(50, 150), table.upper()
                           , tables[table]["location"].title()))
     tables[table]["seated"] = 0
     tables[table]["customer"] = ""
-    print(threading.current_thread().getName(), 'Exiting')
+    # logging.debug('Exiting')
+
 
 # seatCustomer(): Assigns a customer to a table, and changes the table's availability for future customers
 def seatCustomer(table, customer, partySize):
@@ -148,14 +158,25 @@ def seatCustomer(table, customer, partySize):
     tables[table]["customer"] = customer
     tables[table]["seated"] = partySize
     # print(tables[table])
-    print("Wonderful. We've placed you at table {}, {}. Enjoy your meal.\n\n".format(table, customer.title()))
+    print("Wonderful. We've placed you at table {}, {}. Enjoy your meal.\n\n".format(table.upper(), customer.title()))
+    printQueuedMsgs()
 
-    t = threading.Timer(random.randint(3, 10), checkout, [table])
+    t = threading.Timer(random.randint(5, 15), checkout, [table])
     threads.append(t)
+    # print(threads)
     t.start()
 
 
+# saveTables(): Exports customers currently in the restaurant onto a text file.
+# Assumes customers get locked int the restaurant until it reopens.
+def saveTables():
+    file = open()
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format='[%(levelname)s] (%(threadName)-10s) %(message)s',
+                        )
+
     # First, let's establish a dictionary of tables and presumed placements within the restaurant.
     # ready: seats that are ready for a customer (assume ready is fixed)
     # seated: seats currently occupied by a customer (cannot exceed ready)
@@ -175,18 +196,24 @@ if __name__ == '__main__':
         'mezzanine': 12
     }
     alerts = []
-    threads  = []
+    threads = []
 
     print('\n\n\n\n\n')
 
     while True:
-        name = str(input("Good evening, what is your name: "))
+        name = str(input("Good evening, what is your name (0 to exit): "))
+        if name == '0':
+            sys.exit()
         print("Thank you, " + name.title() + ".")
+        printQueuedMsgs()
+
         partySize = takePartySize()
         # Loop to take user input for place. Will continue until party size matches location preference conditions.
         while True:
             preference = str(input("\nWhere would you like to be seated tonight? You can type 'help' to see our options, "
-                                   "or enter 'any' if you do not have a preference: ")).lower()
+                                   "or enter 'any' if you do not have a preference (0 to exit): ")).lower()
+            if preference == '0':
+                sys.exit()
             if preference == 'help':
                 printAllRooms()
             else:
